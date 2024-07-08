10e34cfd3b8ca53d3a575839392a5b61455ba5e8
Ivan Barankov <barankov96@bk.ru>
2024-07-08 16:18:32 +0300

    updated help_command_handler

--- cloobot.py
+++ cloobot.py
@@ -4,6 +4,14 @@ import config
 bot = telebot.TeleBot(config.BOT_TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(message.from_user.id, f'Привет, {message.from_user.last_name}!')
+
+@bot.message_handler(commands=['help'])
+def start_handler(message):
+    bot.send_message(message.from_user.id, f'Чем я могу помочь?')
+
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
     if message.text == "Привет":
